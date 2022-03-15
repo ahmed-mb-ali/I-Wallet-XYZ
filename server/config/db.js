6dcce934fc84f097367d0bc7b8343193a00dacd3
Ahmed <ahm3d>
2022-03-15 12:18:37 -0400

    update the database

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,11 @@
 module.exports =
 {
+   
+    // new database 
+    "URI": "mongodb+srv://AhmedAli:aGSI9lpQsJC2C4vK@mongodbserver.emhjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+
     //"URI": "lmongodb://localhost/app_user"
-    "URI": "mongodb+srv://ahmed:euRWSSjBBgO5o5Ua@mongodbserver.ujiiu.mongodb.net/app_user?retryWrites=true&w=majority"
+    //"URI": "mongodb+srv://ahmed:euRWSSjBBgO5o5Ua@mongodbserver.ujiiu.mongodb.net/app_user?retryWrites=true&w=majority"
 }
